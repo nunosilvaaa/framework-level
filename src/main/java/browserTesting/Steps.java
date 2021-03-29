b4bffcac74e35e58c7f1fd338e2170af652c8c91
Rui Braz Rodrigues <rui.braz.rodrigues@celfocus.com>
2021-03-29 15:07:18 +0100

    Add: cucumber steps to ui tests

---
+++ src/main/java/browserTesting/Steps.java
@@ -0,0 +1,51 @@
+package browserTesting;
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Steps {
+
+    WebDriver driver;
+
+    @Given("^I open Chrome and launch the application$")
+    public void openChromeAndLaunchApplication()
+    {}
+
+    @When("^I select type \"(.*)\"$")
+    public void selectType(String type)
+    {
+        Select select = new Select(driver.findElement(By.id("type")));
+        select.selectByVisibleText(type);
+    }
+
+    @When("^I select support plan \"(.*)\"$")
+    public void selectSupportPlan(String plan)
+    {
+        Select select = new Select(driver.findElement(By.id("support")));
+        select.selectByVisibleText(plan);
+    }
+
+    @When("^I write monthly duration of \"(.*)\"$")
+    public void writeMonthlyDuration (String duration)
+    {
+        driver.findElement(By.id("duration")).sendKeys(duration);
+    }
+
+    @When("^I click in calculate price button")
+    public void clickCalculatePriceButton()
+    {
+        driver.findElement(By.id("calculate")).click();
+    }
+
+    @Then("^I validate price is \"(.*)\"$")
+    public void	validatePrice(String price) throws InterruptedException {
+        Thread.sleep(5*1000);
+        Assert.assertEquals(price, driver.findElement(By.id("price")).getText());
+    }
+}
