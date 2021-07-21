55e5956acdd0867da007a0ad3176bf8d29d217f1
nuno.n.silva <nuno.n.silva@vision-box.com>
2021-07-21 18:47:02 +0100

    Added StepDefinitions for existing features/scenarios

--- src/main/java/browserTesting/Steps.java
+++ src/main/java/browserTesting/Steps.java
@@ -1,21 +1,52 @@
 package browserTesting;
 
-
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import webServiceTesting.models.Root;
+import webServiceTesting.utils.Globals;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class Steps {
 
-    WebDriver driver;
-
+	private static Root configRoot = Globals.getWebTestingObject();
+    private static WebDriver driver;
+    Properties properties = new Properties();
+    
+    @After
+    public void tearDown(){
+        if (driver != null)
+        driver.quit();
+    }
+    
     @Given("^I open Chrome and launch the application$")
-    public void openChromeAndLaunchApplication()
-    {}
+    public void openChromeAndLaunchApplication() throws InterruptedException
+    {
+    	//Setting system properties of ChromeDriver
+    	System.setProperty("webdriver.chrome.driver", configRoot.staticPaths.chromeDriverPath);
+    	
+    	//Creating an object of ChromeDriver
+    	driver = new ChromeDriver();
+    	driver.manage().window().maximize();
+    	
+    	//Specifying pageLoadTimeout and Implicit wait
+    	driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
+    	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    	
+    	//launching the specified URL
+    	driver.get(configRoot.staticPaths.productSubscriptionPageURL);
+    	
+    	Globals.waitForLoad(driver);
+    }
 
     @When("^I select type \"(.*)\"$")
     public void selectType(String type)
@@ -45,7 +76,7 @@ public class Steps {
 
     @Then("^I validate price is \"(.*)\"$")
     public void	validatePrice(String price) throws InterruptedException {
-        Thread.sleep(5*1000);
+        Globals.waitForLoad(driver, "return document.getElementById('loader').classList.contains('d-none')");
         Assert.assertEquals(price, driver.findElement(By.id("price")).getText());
     }
 
--- src/main/java/webServiceTesting/Steps.java
+++ src/main/java/webServiceTesting/Steps.java
@@ -1,31 +1,94 @@
 package webServiceTesting;
 
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import webServiceTesting.models.Root;
+import webServiceTesting.utils.Globals;
 
 public class Steps {
 
-  CreateUser createUser;
-  String name, job;
+  private static Root configRoot = Globals.getWebTestingObject();
+  private static String usersPath = configRoot.webTesting.baseUri+configRoot.webTesting.basePaths.usersPath;
+  private static String registerPath = configRoot.webTesting.baseUri+configRoot.webTesting.basePaths.registerPath;
+  private CreateUser createUser;
+  private RegisterUser registerUser;
 
-  @Given("^I use user creation service$")
+  @Given("^I create the user creation service$")
   public void useUserCreationWebService() {
     createUser = new CreateUser();
+  }
+  
+  @Given("^I create the user registration service$")
+  public void useUserRegistrationWebService() {
+    registerUser = new RegisterUser();
   }
 
   @When("^I set name \"([^\"]*)\"$")
   public void setName(String name) {
     createUser.setName(name);
-    this.name = name;
   }
-
+  
   @When("^I set job \"([^\"]*)\"$")
   public void setJob(String job) {
+	createUser.setJob(job);
   }
 
-  @Then("^I validate my response is correct$")
+  @When("^I set email \"([^\"]*)\"$")
+  public void setEmail(String email) {
+	registerUser.setEmail(email);
+  }
+  
+  @When("^I set password \"([^\"]*)\"$")
+  public void setPassword(String password) {
+	registerUser.setPassword(password);
+  }
+  
+  @Then("^I submit the user deletion request$")
+  public void executeAddUserOperation() {
+	  createUser.buildBody();;
+  }
+  
+  @Then("^I submit the user registration request$")
+  public void executeRegisterUserOperation() {
+	registerUser.buildBodyWithEmail();
+  }
+  
+  @Then("^I submit the user creation request$")
+  public void executeDeleteUserOperation() {
+	  createUser.buildBody();
+  }
+
+  @Then("^I validate the user has been created$")
   public void validateMyResponseIsCorrect() {
+  createUser.getRequestSpecification()
+	.given()
+		.body(createUser.buildBody().toJSONString())
+	.when()
+		.post(usersPath)
+	.then()
+		.statusCode(201);
+  }
+  
+  @Then("^I validate the user has been deleted$")
+  public void validateUserHasBeenDeleted() {
+    createUser.getRequestSpecification()
+    .given()
+    .when()
+		.delete(usersPath+"/2")
+	.then()
+		.statusCode(204);
+  }
+  
+  @Then("^I validate the user registration was not successful$")
+  public void validateUserRegistrationWasNotSuccessful() {
+    registerUser.getRequestSpecification()
+    .given()
+		.contentType("application/json")
+		.body(registerUser.buildBodyWithEmail().toJSONString())
+	.when()
+		.post(registerPath)
+	.then()
+		.statusCode(400);
   }
 }
